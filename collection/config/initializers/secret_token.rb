b63b73cf0b6968b5e1453361a0a01dfce681da38
Philip Hale <p.hale.09@aberdeen.ac.uk>
2011-04-27 12:26:03 +0100

    basics in place - ready to start writing the app

---
+++ collection/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collection::Application.config.secret_token = '4256c412d2e86efa9d2ed612c81e85a1656f60d5c35a53f0b6f73bc83cd2f5c8a0eaf0edb5ae45438a6cc42803ca875efcb63229e1fd2b7360ac6d6b8d547eab'
