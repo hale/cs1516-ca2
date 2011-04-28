25638394c25653978742c3aa4de61bb1c127c0c2
Philip Hale <p.hale.09@aberdeen.ac.uk>
2011-04-28 18:54:51 +0100

    up to step 11.  see spec file comments for TODO.

--- collection/spec/controllers/items_controller_spec.rb
+++ collection/spec/controllers/items_controller_spec.rb
@@ -6,5 +6,11 @@ describe ItemsController do
 			get :new
 			response.should be_success
 		end
+		
+		# Need to implement 'it should redirect to the new cd/dvd/book page' for each option in the list.
+		# Maybe need to change the above 'describe' because i'm not sure we ever create a new item.
+		# This controller is just for redirecting to one of three controllers.
+		
+				
 	end
 end
